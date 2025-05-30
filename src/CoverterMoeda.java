4a48f4eb8361a7d11c9b9b2fd856f51f2611f26f
Maria Viana do Nascimento <mariaviana05091997@gmail.com>
2025-05-30 11:55:57 -0300

    lógica de conversão e chamada à API

--- src/CoverterMoeda.java
+++ src/CoverterMoeda.java
@@ -1,2 +1,14 @@
+
 public class CoverterMoeda {
+    private final ExchangeRateService service = new ExchangeRateService();
+
+    public void convertermoeda(String from, String to, double valor) {
+        try {
+            double rate = service.obterTaxa(from, to);
+            double resultado = valor * rate;
+            System.out.printf("%.2f %s = %.2F %s%n", valor, from, resultado, to);
+        } catch (Exception e) {
+            System.out.println("Erro ao converter moeda: " + e.getMessage());
+        }
+    }
 }
